fe8119ce7bda4e750e606719e77c28da45f0a5e3
Terry Lewis <terrylewis@outlook.com>
2014-06-08 18:07:04 -0500

    Initial Commit

---
+++ ImStructures/ImStructures.h
@@ -0,0 +1,19 @@
+//
+//  ImStructures.h
+//  ImStructures
+//
+//  Created by Terry Lewis II on 6/8/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ImStructures.
+FOUNDATION_EXPORT double ImStructuresVersionNumber;
+
+//! Project version string for ImStructures.
+FOUNDATION_EXPORT const unsigned char ImStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImStructures/PublicHeader.h>
+
+
